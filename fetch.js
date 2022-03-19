f6e4a78c3c0540f77c52452b9f028fd85265353e
felix <josevelazco134@gmail.com>
2022-03-18 18:07:17 -0600

    Update: missing image

--- fetch.js
+++ fetch.js
@@ -43,8 +43,8 @@ const fetchPokemon = ()=>
     fetch(url).then(function(result){
         if(result.status != "200"){
             console.log("Pokemon no identificado");
-            pokeImg.src = "/assets/missing-no.png";
-            //pokeImg.src = "https://raw.githubusercontent.com/felixVelazco/pokedex/main/assets/missing-no.png";
+            //pokeImg.src = "/assets/missing-no.png";
+            pokeImg.src = "https://github.com/felixVelazco/pokedex/blob/main/assets/missing-no.png";
             pokeName.innerText = '???';
             pokeID.innerText = '???';
             type1.innerText = '???';
